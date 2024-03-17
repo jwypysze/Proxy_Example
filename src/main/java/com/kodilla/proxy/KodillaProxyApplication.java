0db3001394cd3b5f41278d9fb45591a5abef806f
jwypysze <joanna.wypyszewska@gmail.com>
2024-03-17 13:57:41 +0100

    Proxy - example

---
+++ src/main/java/com/kodilla/proxy/KodillaProxyApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodillaProxyApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/kodilla/proxy/db/DbDataRetriever.java
@@ -0,0 +1,9 @@
+package com.kodilla.proxy.db;
+
+public interface DbDataRetriever {
+
+    int getFirstValue() throws InterruptedException;
+    int getSecondValue() throws InterruptedException;
+    int getThirdValue() throws InterruptedException;
+
+}
